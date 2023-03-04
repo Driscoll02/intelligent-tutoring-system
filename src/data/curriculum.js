80a2a6fc2a97a47918a532dac9949ea2fc09f077
Joseph Driscoll <driscoll2507@gmail.com>
2023-03-04 11:48:50 +0000

    Populated year 1 curriculum with questions and answers. Added images to support questions.

--- src/data/curriculum.js
+++ src/data/curriculum.js
@@ -7,16 +7,24 @@ export const curriculumData = [
                 tag: "number and place value",
                 questions: [
                     {
-                        question: [],
-                        answer: []
+                        question: ["What number is 1 more than 4?"],
+                        answer: ["5"]
                     },
                     {
-                        question: [],
-                        answer: []
+                        question: ["What number is 5 more than 5?"],
+                        answer: ["10"]
                     },
                     {
-                        question: [],
-                        answer: []
+                        question: ["What number is 2 less than 10?"],
+                        answer: ["8"]
+                    },
+                    {
+                        question: ["Write all the numbers from 1 to 10"],
+                        answer: ["1, 2, 3, 4, 5, 6, 7, 8, 9, 10", "1 2 3 4 5 6 7 8 9 10", "12345678910"]
+                    },
+                    {
+                        question: ["Put these numbers in order: 5, 16, 18, 2, 3"],
+                        answer: ["2, 3, 5, 16, 18", "2 3 5 16 18", "2351618"]
                     }
                 ]
             },
@@ -24,16 +32,28 @@ export const curriculumData = [
                 tag: "addition and subtraction",
                 questions: [
                     {
-                        question: [],
-                        answer: []  
+                        question: ["5 + 4"],
+                        answer: ["9"]  
                     },
                     {
-                        question: [],
-                        answer: []  
+                        question: ["10 - 6"],
+                        answer: ["4"]  
                     },
                     {
-                        question: [],
-                        answer: []  
+                        question: ["18 - 5"],
+                        answer: ["13"]  
+                    },
+                    {
+                        question: ["15 + 2"],
+                        answer: ["17"]  
+                    },
+                    {
+                        question: ["1 + 3"],
+                        answer: ["4"]  
+                    },
+                    {
+                        question: ["20 - 6"],
+                        answer: ["14"]  
                     }
                 ]
             },
@@ -41,16 +61,44 @@ export const curriculumData = [
                 tag: "multiplication and division",
                 questions: [
                     {
-                        question: [],
-                        answer: []  
+                        question: ["2 x 3"],
+                        answer: ["6"]  
                     },
                     {
-                        question: [],
-                        answer: []  
+                        question: ["10 x 2"],
+                        answer: ["20"]  
                     },
                     {
-                        question: [],
-                        answer: []  
+                        question: ["10 x 3"],
+                        answer: ["30"]  
+                    },
+                    {
+                        question: ["5 x 2"],
+                        answer: ["10"]  
+                    },
+                    {
+                        question: ["5 x 3"],
+                        answer: ["15"]  
+                    },
+                    {
+                        question: ["10 \u00F7 2"],
+                        answer: ["5"]  
+                    },
+                    {
+                        question: ["6 \u00F7 2"],
+                        answer: ["3"]  
+                    },
+                    {
+                        question: ["20 \u00F7 10"],
+                        answer: ["2"]  
+                    },
+                    {
+                        question: ["5 \u00F7 1"],
+                        answer: ["5"]  
+                    },
+                    {
+                        question: ["10 \u00F7 5"],
+                        answer: ["2"]  
                     }
                 ]
             },
@@ -58,16 +106,19 @@ export const curriculumData = [
                 tag: "fractions",
                 questions: [
                     {
-                        question: [],
-                        answer: []  
+                        image: "../images/fractions/halfFractionLarge.png",
+                        question: ["What is this fraction?"],
+                        answer: ["one half", "a half", "1/2"]
                     },
                     {
-                        question: [],
-                        answer: []  
+                        image: "../images/fractions/quarterFractionLarge.png",
+                        question: ["What is this fraction?"],
+                        answer: ["one quarter", "a quarter", "1/4"] 
                     },
                     {
-                        question: [],
-                        answer: []  
+                        image: "../images/fractions/threeQuarterFractionLarge.png",
+                        question: ["What is this fraction?"],
+                        answer: ["three quarters", "3/4"]
                     }
                 ]
             },
@@ -75,16 +126,34 @@ export const curriculumData = [
                 tag: "measurement",
                 questions: [
                     {
-                        question: [],
-                        answer: []  
+                        question: ["Which of these is a way to describe length?"],
+                        multipleChoiceAnswers: ["heavy", "earlier", "long", "empty"],
+                        answer: ["long"]  
                     },
                     {
-                        question: [],
-                        answer: []  
+                        question: ["Which of these is a way to describe weight?"],
+                        multipleChoiceAnswers: ["short", "slower", "half", "heavy"],
+                        answer: ["heavy"]  
                     },
                     {
-                        question: [],
-                        answer: []  
+                        question: ["Which of these is a way we measure time?"],
+                        multipleChoiceAnswers: ["years", "full", "lighter", "empty"],
+                        answer: ["years"]  
+                    },
+                    {
+                        image: "../images/clocks/930clock.png",
+                        question: ["What time does this clock show?"],
+                        answer: ["9:30", "nine thirty", "9 30", "9-30", "half 9", "half past 9", "30 minutes past 9", "30 minutes to 10", "thirty minutes past nine", "thirty minutes to ten"]  
+                    },
+                    {
+                        image: "../images/clocks/400clock.png",
+                        question: ["What time does this clock show?"],
+                        answer: ["4:00", "four oclock", "four o clock", "four o'clock", "4 00", "4-00"]  
+                    },
+                    {
+                        image: "../images/clocks/630clock.png",
+                        question: ["What time does this clock show?"],
+                        answer: ["6:30", "six thirty", "6 30", "6-30", "half 6", "half past 6", "30 minutes past 6", "30 minutes to 7", "thirty minutes past six", "thirty minutes to seven"]  
                     }
                 ]
             },
@@ -92,16 +161,24 @@ export const curriculumData = [
                 tag: "geometry - properties of shapes",
                 questions: [
                     {
-                        question: [],
-                        answer: []  
+                        image: "../images/shapes/Circle.png",
+                        question: ["What is this shape?"],
+                        answer: ["circle"]
                     },
                     {
-                        question: [],
-                        answer: []  
+                        image: "../images/shapes/Square.png",
+                        question: ["What is this shape?"],
+                        answer: ["square"]
                     },
                     {
-                        question: [],
-                        answer: []  
+                        image: "../images/shapes/Triangle.png",
+                        question: ["What is this shape?"],
+                        answer: ["triangle"]
+                    },
+                    {
+                        image: "../images/shapes/Rectangle.png",
+                        question: ["What is this shape?"],
+                        answer: ["rectangle"]
                     }
                 ]
             },
@@ -109,16 +186,19 @@ export const curriculumData = [
                 tag: "geometry - position and direction",
                 questions: [
                     {
-                        question: [],
-                        answer: []  
+                        image: "../images/shapes/QuarterRightTriangle.png",
+                        question: ["how has this shape been rotated?"],
+                        answer: ["quarter right", "90 degree right", "quarter left", "90 degree left"]
                     },
                     {
-                        question: [],
-                        answer: []  
+                        image: "../images/shapes/FlippedTriangle.png",
+                        question: ["how has this shape been rotated?"],
+                        answer: ["left", "right", "180 degree left", "180 degree right"]
                     },
                     {
-                        question: [],
-                        answer: []  
+                        image: "../images/shapes/QuarterRightRectangle.png",
+                        question: ["how has this shape been rotated?"],
+                        answer: ["quarter right", "90 degree right", "quarter left", "90 degree left"] 
                     }
                 ]
             }
@@ -773,4 +853,12 @@ export const curriculumData = [
     ]
 ]
 
-// https://www.gov.uk/government/publications/national-curriculum-in-england-mathematics-programmes-of-study/national-curriculum-in-england-mathematics-programmes-of-study
+// KS1 & KS2 curriculum: https://www.gov.uk/government/publications/national-curriculum-in-england-mathematics-programmes-of-study/national-curriculum-in-england-mathematics-programmes-of-study
+// Resource used to make fraction images: https://teachablemath.com/apps/fraction-shape-maker/
+// Resource used to make analogue clock images: https://helpingwithmath.com/generators/3md1-clock-face-generator01/
+
+// Shape colours:
+// inner: #37c220
+// outline: #1b1313
+// fraction red: #dd3333
+// rectangle dimensions: w:600 h:200
